88e98fdb0936228bd2b248e1581e2274eeb76487
JoaocGuerra <jcarlos.guerra@hotmail.com>
2022-03-20 14:54:16 -0300

    Removing package that it's not working

--- app/React App/src/database/index.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/database'
-import firebaseConfig from './config'
-
-const  firebaseApp = firebase.initializeApp(firebaseConfig)
-
-export default firebaseApp; 
--- app/React App/src/utils/date.ts
+++
@@ -1,5 +0,0 @@
-export const dateFormatter = new Intl.DateTimeFormat("pt-BR", {
-  month: "long",
-  day: "2-digit",
-  year: "numeric",
-});
